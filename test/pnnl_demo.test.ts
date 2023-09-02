2ab1c11f3972f8747994f5f2defa80822452fddf
David Kocen <dkocen0@gmail.com>
2023-09-02 16:02:20 -0700

    Initial commit

---
+++ test/pnnl_demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PnnlDemo from '../lib/pnnl_demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pnnl_demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PnnlDemo.PnnlDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
